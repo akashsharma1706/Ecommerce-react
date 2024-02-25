55c9b175e20b40ea13bf3b2e31946932b8a8fdee
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-02-25 09:13:55 +0530

    Add files via upload

--- AuthContext.js
+++ AuthContext.js
@@ -1,17 +1,21 @@
 // AuthContext.js
 import React, { createContext, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const history = useHistory();
 
   const login = (token) => {
     setToken(token);
+    history.push('/profile'); // Redirect to profile page after login
   };
 
   const logout = () => {
     setToken(null);
+    history.push('/login'); // Redirect to login page after logout
   };
 
   return (
